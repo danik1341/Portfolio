4826325128e505409b932ee0b2cdc0659dc90911
Daniel <111450120+danik1341@users.noreply.github.com>
2023-03-07 15:59:33 +0200

    added functionality for project section

--- script.js
+++ script.js
@@ -4,7 +4,18 @@ const show = function(shown,hidden) {
     return false;
 }
 const goToAddi = (page) => {
-    if(document.getElementById(page).style.display='flex'){
+    if(document.getElementById(page).style.display==='flex'){
         document.getElementById('about_additional').scrollIntoView({behavior: 'smooth'});
     }   
+}
+const goToProj = (page) => {
+    if(document.getElementById(page).style.display==='flex'){
+        document.getElementById('project_detail_page').scrollIntoView({behavior: 'smooth'});
+    } 
+}
+const goToProjRecent = () => {
+    if(document.getElementById('project_section').style.display==='flex'){
+        show('project_detail_page','project_section');
+    }
+    document.getElementById('project_detail_page').scrollIntoView({behavior: 'smooth'});
 }
